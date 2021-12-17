d79decd037ba95bb20e617c63237db583e70897d
hollyjanedalton <hjdalton89@gmail.com>
2021-12-17 14:58:48 -0500

    Added APIkey and Url

--- src/script.js
+++ src/script.js
@@ -1,1 +1,4 @@
-alert("Let's code 🤠");
+let apiKey = "d21c60e862cea62dbdf3c4bc5698f1d3";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}`;
+
+console.log(apiUrl);
