b012b3a894685dd59775ed3b81bdd55273dd6137
hollyjanedalton <hjdalton89@gmail.com>
2021-12-17 15:56:06 -0500

    Showing the current temperature

--- src/script.js
+++ src/script.js
@@ -1,4 +1,18 @@
-let apiKey = "d21c60e862cea62dbdf3c4bc5698f1d3";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}`;
+function displayTemperature(response) {
+  console.log(response.data);
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+}
 
-console.log(apiUrl);
+let apiKey = "d21c60e862cea62dbdf3c4bc5698f1d3";
+let apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=London&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayTemperature);
